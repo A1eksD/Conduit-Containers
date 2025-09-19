b131280f96285e8de5a95121dc639b9cce091d72
Aleks <143895611+A1eksD@users.noreply.github.com>
2025-09-19 23:26:58 +0200

    changed some settings

--- conduit-backend/conduit/settings.py
+++ conduit-backend/conduit/settings.py
@@ -26,7 +26,7 @@ environ.Env.read_env(BASE_DIR / ".env")
 SECRET_KEY= env("SECRET_KEY")
 DEBUG = env.bool("DEBUG")
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS") 
-
+CORS_ALLOWED_ORIGINS  = env.list("CORS_ALLOWED_ORIGINS")
 
 # Application definition
 
